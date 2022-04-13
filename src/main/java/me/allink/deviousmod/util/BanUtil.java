4674c1aa89bb49b6694f3675dbcef19c741067a9
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2022-04-13 06:21:32 +0200

    Make ban and kick work without command blocks
    was long needed

--- src/main/java/me/allink/deviousmod/util/BanUtil.java
+++ src/main/java/me/allink/deviousmod/util/BanUtil.java
@@ -28,91 +28,21 @@ public class BanUtil {
     }
 
     /**
-     * Kick the requested player from the server (requries OP & command blocks)
+     * Kick the requested player from the server (requries OP)
      *
      * @param identifier Who to kick
      */
     public void kickPlayer(String identifier) {
-        commandBlockUtil.placeRepeating(String.format("/execute as %s run title @s title [{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"},{\"nbt\":\"\",\"entity\":\"@e[limit=20]\"}]", identifier));
-    }
-
-    private void doTheBanning(UUID uuid) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        ClientPlayerInteractionManager interactionManager = client.interactionManager;
-        int previousSlot = client.player.getInventory().selectedSlot;
-        NbtCompound entityTag = new NbtCompound();
-        String devious = "devious";
-        entityTag.putString("id", "minecraft:chest_minecart");
-        entityTag.putString("LootTable", devious.repeat((256 / devious.length()) + 1));
-        entityTag.putUuid("UUID", uuid);
-        entityTag.putByte("CustomDisplayTile", (byte) 1);
-        entityTag.putInt("DisplayOffset", 6);
-        NbtCompound displayState = new NbtCompound();
-        displayState.putString("Name", "minecraft:tnt");
-        entityTag.put("DisplayState", displayState);
-        NbtCompound rootNbt = new NbtCompound();
-        rootNbt.putString("id", "minecraft:cat_spawn_egg");
-        NbtCompound tag = new NbtCompound();
-        tag.put("EntityTag", entityTag);
-        rootNbt.put("tag", tag);
-        rootNbt.putByte("Count", (byte) 1);
-        ItemStack itemStack = ItemStack.fromNbt(rootNbt);
-        System.out.println(rootNbt);
-        interactionManager.clickCreativeStack(itemStack, 37);
-        Timer t = new Timer();
-        t.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                client.player.getInventory().selectedSlot = 1;
-                interactionManager.interactBlock(client.player, client.world, Hand.MAIN_HAND, new BlockHitResult(client.player.getPos().subtract(0, 2, 0), Direction.NORTH, client.player.getBlockPos().subtract(new Vec3i(0, 2, 0)), false));
-                interactionManager.clickCreativeStack(ItemStack.EMPTY, 37);
-                client.player.getInventory().selectedSlot = previousSlot;
-            }
-        }, 110L + 55L);
+        MinecraftClient.getInstance().player.sendChatMessage("/kick " + identifier);
     }
 
 
     /**
-     * Ban the requested player from the server (requries OP & command blocks)
+     * Ban the requested player from the server (requries OP)
      *
      * @param identifier Who to ban
      */
     public void banPlayer(String identifier) {
-        this.kickPlayer(identifier);
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (DeviousModClient.uuidCache.containsKey(identifier)) {
-                    doTheBanning(DeviousModClient.uuidCache.get(identifier));
-                } else {
-                    if (identifier.contains("-")) { // is uuid
-                        doTheBanning(UUID.fromString(identifier));
-                    } else {
-                        if (DeviousModClient.isOffline) {
-                            doTheBanning(UUID.nameUUIDFromBytes(String.format("OfflinePlayer:%s", identifier).getBytes()));
-                        } else {
-                            Thread t = new Thread(() -> {
-                                HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.ashcon.app/mojang/v2/user/" + identifier))
-                                        .GET()
-                                        .build();
-
-                                CompletableFuture<HttpResponse<String>> responseFuture = DeviousModClient.CLIENT.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-                                HttpResponse<String> response;
-                                try {
-                                    response = responseFuture.get();
-
-                                    AshconPlayer player = new Gson().fromJson(response.body(), AshconPlayer.class);
-                                    doTheBanning(UUID.fromString(player.uuid));
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            });
-                            t.start();
-                        }
-                    }
-                }
-            }
-        }, 150 + 55L);
+        MinecraftClient.getInstance().player.sendChatMessage("/ban " + identifier);
     }
 }
